29451f05e0f553c080748bb4b21839d89a9de59f
blacktrub <lightlocdog@gmail.com>
2017-08-08 15:22:16 +0300

    install jinja2 template package

--- utils.py
+++ utils.py
@@ -5,6 +5,6 @@ def logging_view(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         request = args[0]
-        print(f'fetch request from {request.url}')
+        print(f'got request from {request.url}')
         return func(*args, **kwargs)
     return wrapper
